96b494c9538e4d55d69a0769bf1c85bb17a85632
Connie Hilarides <conni_h@outlook.com>
2019-12-05 23:19:52 -0800

    Benchmark day 6

--- src/bin/day06.rs
+++ src/bin/day06.rs
@@ -1,3 +1,7 @@
+#![cfg_attr(test, feature(test))]
+#[cfg(test)]
+extern crate test;
+
 use pathfinding::prelude::dijkstra;
 use std::collections::HashMap;
 
@@ -5,13 +9,15 @@ static INPUT: &str = include_str!("input/day06.txt");
 
 type Id = &'static str;
 
+// Parse the orbits from the input file, it's just a (orbited body, orbiter)
+// pair for every orbit relationship.
 fn orbits() -> Vec<(Id, Id)> {
-    let mut orbits = Vec::with_capacity(1100);
-    for line in INPUT.lines() {
-        if line.len() < 7 {
-            continue;
-        }
+    // Save an allocation~ that's about how long the file is
+    let mut orbits = Vec::with_capacity(1062);
 
+    // lines() will spare us the final empty line :)
+    for line in INPUT.lines() {
+        // The bodies are all 3 character IDs, with a ) separating them
         let a = &line[0..3];
         let b = &line[4..7];
         orbits.push((a, b));
@@ -28,7 +34,9 @@ struct Node {
 }
 
 fn orbit_tree(orbits: &[(Id, Id)]) -> OrbitTree {
-    let mut tree = OrbitTree::with_capacity(orbits.len());
+    // This is apparently the exact capacity that will always be needed for
+    // the exact size of our tree.
+    let mut tree = OrbitTree::with_capacity(1792);
 
     for &(a, b) in orbits {
         let node_a = tree.entry(a).or_default();
@@ -78,4 +86,48 @@ fn main() {
         .expect("There is no route from you to santa :(");
 
     println!("Part 2: {}", cost);
+}
+
+#[cfg(test)]
+#[bench]
+fn parsing(bench: &mut test::Bencher) {
+    bench.iter(|| {
+        let orbits = orbits();
+        test::black_box(&orbits);
+    });
+}
+
+#[cfg(test)]
+#[bench]
+fn tree_building(bench: &mut test::Bencher) {
+    let orbits = orbits();
+    bench.iter(|| {
+        let tree = orbit_tree(&orbits);
+        test::black_box(&tree);
+    });
+}
+
+#[cfg(test)]
+#[bench]
+fn suborbit_counting(bench: &mut test::Bencher) {
+    let orbits = orbits();
+    let tree = orbit_tree(&orbits);
+    bench.iter(|| {
+        test::black_box(count_suborbits(&tree, "COM", 1));
+    });
+}
+
+#[cfg(test)]
+#[bench]
+fn pathfinding(bench: &mut test::Bencher) {
+    let orbits = orbits();
+    let tree = orbit_tree(&orbits);
+    bench.iter(|| {
+        let start = tree["YOU"].parent.expect("YOU must have a parent");
+        let end = tree["SAN"].parent.expect("SAN must have a parent");
+
+        let (_path, cost) = dijkstra(&start, |&n| adjacent_bodies(n, &tree), |n| *n == end)
+            .expect("There is no route from you to santa :(");
+        test::black_box(cost);
+    });
 }
