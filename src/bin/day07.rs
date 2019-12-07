5f0859b6004790c49937e1cc72a9d2084fdbbe3b
Connie Hilarides <conni_h@outlook.com>
2019-12-07 14:03:27 -0800

    Final cleanup

--- src/bin/day07.rs
+++ src/bin/day07.rs
@@ -14,20 +14,22 @@ fn try_sequence<'a>(seq: impl Iterator<Item = &'a i32>) -> i32 {
     // Create a channel pair for each CPU
     for &phase in seq {
         let (tx, rx) = channel();
-        tx.send(phase).expect("um");
+        tx.send(phase).unwrap();
         senders.push(tx);
         receivers.push(rx);
     }
 
     // Send the initial input to the first CPU
-    senders[0].send(0).expect("um");
+    senders[0].send(0).unwrap();
 
     // Rotate the senders so that CPU A's output will send to CPU B's input
     senders.rotate_left(1);
 
-    // Construct the CPUs in a feedback loop
+    // Construct the CPUs in a feedback loop. The feedback loop
+    // will be ignored for part 1 as each CPU will merely halt
+    // when it computes its result.
     let mut cpus = Vec::with_capacity(5);
-    for (rx, tx) in receivers.drain(..).zip(senders.drain(..)) {
+    for (rx, tx) in receivers.into_iter().zip(senders) {
         cpus.push(Cpu::parse_with_io(INPUT, ChannelIO::new(rx, tx)));
     }
 
@@ -42,6 +44,7 @@ fn try_sequence<'a>(seq: impl Iterator<Item = &'a i32>) -> i32 {
 }
 
 fn find_max_sequence(phases: &[i32]) -> i32 {
+    // Get the output for each permutation of phases, and choose the largest
     permutations_of(phases).map(try_sequence).max().unwrap()
 }
 
